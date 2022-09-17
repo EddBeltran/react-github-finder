047e85fef53a9e6c22957498e9c176f0536a0a7a
Eduardo Beltran <edd.bng@gmail.com>
2022-09-16 20:04:17 -0500

    Clean context and add a separate functions file

--- src/components/users/UserSearch.js
+++ src/components/users/UserSearch.js
@@ -1,25 +1,27 @@
 import { useState, useContext } from "react";
 import AlertContext from "../../context/alert/AlertContext";
+import { searchUsers } from "../../context/github/GitHubActions";
 import GithubContext from "../../context/github/GithubContext";
 
 function UserSearch() {
   const [text, setText] = useState("");
-  const { users, searchUsers, clearUsers } = useContext(GithubContext);
+  const { users, dispatch } = useContext(GithubContext);
   const { setAlert } = useContext(AlertContext)
 
   const handleChange = (e) => setText(e.target.value);
-  const handleSubmit = (e) => {
+  
+  const handleSubmit = async (e) => {
     e.preventDefault();
     if (text === "") {
       setAlert('Please enter something', 'error')
     } else {
       // to do search users
-      searchUsers(text)
+      dispatch({type: 'SET_LOADING'})
+      const users  = await searchUsers(text);
+      dispatch({type: 'GET_USERS', payload: users})
       setText("");
     }
   };
-
-
 
   return (
     <div className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8">
@@ -44,7 +46,7 @@ function UserSearch() {
       </div>
       {users.length > 0 && (
         <div>
-          <button onClick={clearUsers} className="bg-gray-100 opacity-25 p-2 rounded-lg w-28">
+          <button onClick={ ()=> dispatch({type: 'CLEAR_USERS'}) } className="bg-gray-100 opacity-25 p-2 rounded-lg w-28">
             Clear
           </button>
         </div>
--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -3,88 +3,20 @@ import githubReducer from "./GithubReducer";
 
 const GithubContext = createContext();
 
-const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
-
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
     user: [],
     repos: [],
-    loading: false
-  }
-  const [state, dispatch] = useReducer(githubReducer, initialState)
-  
-  //get initial users (testing propueses)
-  const searchUsers = async (text) => {
-    setLoading()
-    const params = new URLSearchParams({
-      q: text
-    })
-    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-      headers: {
-        Authorization: `token ${GITHUB_TOKEN}`,
-      },
-    });
-    const { items } = await response.json();
-    dispatch({
-        type: 'GET_USERS',
-        payload: items
-    })
+    loading: false,
   };
-
-  const getUser = async (login) => {
-    setLoading()
-    const response = await fetch(`${GITHUB_URL}/users/${login}`, {
-      headers: {
-        Authorization: `token ${GITHUB_TOKEN}`,
-      },
-    });
-    if ( response.status === 404){
-      window.location = '/notfound'
-    }
-    const data = await response.json();
-    dispatch({
-        type: 'GET_USER',
-        payload: data
-    })
-  };
-
-  //get user repos
-  const getUserRepos = async (login) => {
-    setLoading()
-    const params = new URLSearchParams({
-      sort: "created",
-      per_page: 10
-    })
-    const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
-      headers: {
-        Authorization: `token ${GITHUB_TOKEN}`,
-      },
-    });
-    const data = await response.json();
-
-    dispatch({
-        type: 'GET_REPOS',
-        payload: data
-    })
-  };
-
-  const setLoading  = () => dispatch({type: 'SET_LOADING'})
-
-  const clearUsers  = () => dispatch({type: 'CLEAR_USERS'})
+  const [state, dispatch] = useReducer(githubReducer, initialState);
 
   return (
     <GithubContext.Provider
       value={{
-        users: state.users,
-        user: state.user,
-        loading: state.loading,
-        repos: state.repos,
-        searchUsers,
-        getUser,
-        getUserRepos,
-        clearUsers
+        ...state,
+        dispatch
       }}
     >
       {children}
